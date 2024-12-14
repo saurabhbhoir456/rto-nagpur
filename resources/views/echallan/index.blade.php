35fb9b00666130c123003a3cabe9c5fa46868dbf
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2024-12-14 22:34:57 +0530

    display sucess msg for echallan and environment tax

--- resources/views/echallan/index.blade.php
+++ resources/views/echallan/index.blade.php
@@ -3,6 +3,11 @@
 @section('content')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <h1 class="text-3xl font-bold mb-4">Echallans</h1>
+    @if(session()->has('success'))
+    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
+        {{ session()->get('success') }}
+    </div>
+@endif
     <form action="{{ route('echallans.upload') }}" method="post" enctype="multipart/form-data">
         @csrf
         <input type="file" name="csv_file" accept=".csv">
