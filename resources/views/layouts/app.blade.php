18a8f0220c29896aacec626a8b216ec2c8245af8
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2024-12-17 19:24:50 +0530

    spelling mistake in driving license in function toggleDrivingLicenseSubmenu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,7 +124,7 @@
                         </li>
                         <li>
                             <a href="#" class="flex items-center px-4 py-2 text-lg font-semibold bg-gray-700 hover:bg-indigo-600 rounded-lg transition-colors duration-300 ease-in-out" onclick="toggleDrivingLicenseSubmenu()">
-                                Driving Licnese
+                                Driving License
                                 <svg class="ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
@@ -200,7 +200,7 @@
             }
         }
         function toggleDrivingLicenseSubmenu() {
-            var submenu = document.getElementById('riving-license-submenu');
+            var submenu = document.getElementById('driving-license-submenu');
             if (submenu.style.display === 'block') {
                 submenu.style.display = 'none';
             } else {
