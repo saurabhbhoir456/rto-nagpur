ebe28dbe47b6640d0337ad3382eba0436abeaf87
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2024-12-24 12:49:23 +0530

    admin log in on welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,12 @@
                                     >
                                         Log in
                                     </a>
+                                    <a
+                                    href="{{ route('admin.users.index') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Admin Log in
+                                </a>
 
                                 @if (auth()->check() && auth()->user()->is_admin && Route::has('register'))
                                     <a
