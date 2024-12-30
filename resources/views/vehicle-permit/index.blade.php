d8bdeecbc5321e01877c50f8f60fa2748a24661c
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2024-12-30 19:19:10 +0530

    sample csv added

--- resources/views/vehicle-permit/index.blade.php
+++ resources/views/vehicle-permit/index.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <h1 class="text-3xl font-bold mb-4">Vehicle Permits</h1>
+    <div class="flex justify-end mb-4">
+        <a href="{{ asset('sample_csvs\vehicle_permit_template.csv') }}" 
+       download
+       class="inline-block px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+       Click to Download Sample CSV
+        </a>
+        </div>
     @if(session()->has('success'))
     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded mb-4">
         {{ session()->get('success') }}
