449e4575454a42410846b826b332a679bc667762
saurabhbhoir456 <saurabhbhoir456@gmail.com>
2025-01-07 18:42:45 +0530

    added background for dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,16 +6,17 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 w-7/10 h-screen bg-cover bg-center" style="background-image: url('/RTOLight.jpg'); margin: 0 auto;">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                     <div class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
-    <h2 class="text-lg font-bold text-gray-900 dark:text-gray-100">Message Balance</h2>
-    <p class="text-2xl font-bold text-green-500">{{ number_format($balance) }}</p>
-</div>
+                        <h2 class="text-lg font-bold text-gray-900 dark:text-gray-100">Message Balance</h2>
+                        <p class="text-2xl font-bold text-green-500">{{ number_format($balance) }}</p>
+                    </div>
                 </div>
+                
             </div>
         </div>
     </div>
